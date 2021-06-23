ce6ace6e8ae6741926ec0b810001c65ba498e49d
Narek Torosyan <narek.email@gmail.com>
2021-06-23 22:54:17 +0400

    Fix transition test

--- app/src/main/java/io/shiny/telega/TransitionTest.kt
+++ app/src/main/java/io/shiny/telega/TransitionTest.kt
@@ -1,26 +1,24 @@
 package io.shiny.telega
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.transition.ChangeBounds
-import androidx.transition.Scene
-import androidx.transition.Transition
-import androidx.transition.TransitionManager
+import android.view.Gravity
+import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.transition.*
 
 class TransitionTest : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_transition_test)
-        trans()
     }
 
-    fun trans() {
+    fun trans(view: View) {
         val sceneRoot: ViewGroup = findViewById(R.id.scene_root)
         val aScene: Scene = Scene.getSceneForLayout(sceneRoot, R.layout.auth_hello, this)
         val anotherScene: Scene = Scene.getSceneForLayout(sceneRoot, R.layout.auth_wait, this)
-        var transition: Transition = ChangeBounds()
+        var transition: Transition = Slide(Gravity.LEFT)
         TransitionManager.go(anotherScene, transition)
     }
 }
