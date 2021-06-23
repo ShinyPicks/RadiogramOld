2cda6153cf55b9193f0fd58671fd9a2d28b32c69
Narek Torosyan <narek.email@gmail.com>
2021-06-23 16:35:52 +0400

    Add button for test

--- app/src/main/java/io/shiny/telega/MainActivity.kt
+++ app/src/main/java/io/shiny/telega/MainActivity.kt
@@ -1,11 +1,18 @@
 package io.shiny.telega
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun animTestClick(view: View) {
+        val intent = Intent(this, TransitionTest::class.java)
+        startActivity(intent)
     }
 }
